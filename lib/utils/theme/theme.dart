cb41634ff0efd9bf6e60a1198e922de235dbb0a8
jsmile <jsmile@naver.com>
2024-08-26 21:25:50 +0900

    Final Custom Theme & Constans

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -1,5 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:getx_ecommerce_465/utils/theme/custom_themes/bottom_sheet_theme.dart';
+import 'package:getx_ecommerce_465/utils/theme/custom_themes/checkbox_theme.dart';
+import 'package:getx_ecommerce_465/utils/theme/custom_themes/chip_theme.dart';
+import 'package:getx_ecommerce_465/utils/theme/custom_themes/outlined_button_theme.dart';
+import 'package:getx_ecommerce_465/utils/theme/custom_themes/text_field_theme.dart';
 import 'package:getx_ecommerce_465/utils/theme/custom_themes/text_theme.dart';
+
+import 'custom_themes/appbar_theme.dart';
+import 'custom_themes/elevated_button_theme.dart';
 
 class TAppTheme {
   TAppTheme._();
@@ -9,9 +17,15 @@ class TAppTheme {
     fontFamily: 'Poppins',
     brightness: Brightness.light, // for light theme
     primaryColor: Colors.blue,
+    textTheme: TTextTheme.lightTextTheme,
     scaffoldBackgroundColor: Colors.white,
-    textTheme: TTextTheme.lightTextTheem,
-    elevatedButtonTheme: TElevatedButtomTheme.lightElevatedButtonTheme,
+    appBarTheme: TAppBarTheme.lightAppBarTheme,
+    bottomSheetTheme: TBottomSheetTheme.lightBottomSheetTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.lightElevatedButtonTheme,
+    outlinedButtonTheme: TOutlinedButtonTheme.lightOutlinedButtonTheme,
+    chipTheme: TChipTheme.lightChipTheme,
+    checkboxTheme: TCheckboxTheme.lightCheckboxTheme,
+    inputDecorationTheme: TTextFormFieldTheme.lightInputDecorationTheme,
   );
 
   static ThemeData darkTheme = ThemeData(
@@ -19,8 +33,14 @@ class TAppTheme {
     fontFamily: 'Poppins',
     brightness: Brightness.dark, // for light theme
     primaryColor: Colors.blue,
-    scaffoldBackgroundColor: Colors.black,
-    textTheme: TTextTheme.darkTextTheem,
-    elevatedButtonTheme: TElevatedButtomTheme.darkElevatedButtonTheme,
+    textTheme: TTextTheme.darkTextTheme,
+    scaffoldBackgroundColor: Colors.white,
+    appBarTheme: TAppBarTheme.darkAppBarTheme,
+    bottomSheetTheme: TBottomSheetTheme.darkBottomSheetTheme,
+    elevatedButtonTheme: TElevatedButtonTheme.darkElevatedButtonTheme,
+    outlinedButtonTheme: TOutlinedButtonTheme.darkOutlinedButtonTheme,
+    chipTheme: TChipTheme.darkChipTheme,
+    checkboxTheme: TCheckboxTheme.darkCheckboxTheme,
+    inputDecorationTheme: TTextFormFieldTheme.darkInputDecorationTheme,
   );
 }
