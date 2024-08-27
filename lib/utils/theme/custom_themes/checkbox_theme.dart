2ce52da122880bee5034b07778399275194ea360
jsmile <jsmile@naver.com>
2024-08-28 01:21:03 +0900

    TCheckbox 에서 deprecated 된 속정 수정

--- lib/utils/theme/custom_themes/checkbox_theme.dart
+++ lib/utils/theme/custom_themes/checkbox_theme.dart
@@ -8,16 +8,21 @@ class TCheckboxTheme {
 
   /// Customizable Light Text Theme
   static CheckboxThemeData lightCheckboxTheme = CheckboxThemeData(
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(TSizes.xs)),
-    checkColor: MaterialStateProperty.resolveWith((states) {
-      if (states.contains(MaterialState.selected)) {
+    shape:
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(TSizes.xs)),
+    // checkColor: MaterialStateProperty.resolveWith((states) {
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      // if (states.contains(MaterialState.selected)) {
+      if (states.contains(WidgetState.selected)) {
         return TColors.white;
       } else {
         return TColors.black;
       }
     }),
-    fillColor: MaterialStateProperty.resolveWith((states) {
-      if (states.contains(MaterialState.selected)) {
+    // fillColor: MaterialStateProperty.resolveWith((states) {
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      // if (states.contains(MaterialState.selected)) {
+      if (states.contains(WidgetState.selected)) {
         return TColors.primary;
       } else {
         return Colors.transparent;
@@ -27,16 +32,21 @@ class TCheckboxTheme {
 
   /// Customizable Dark Text Theme
   static CheckboxThemeData darkCheckboxTheme = CheckboxThemeData(
-    shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(TSizes.xs)),
-    checkColor: MaterialStateProperty.resolveWith((states) {
-      if (states.contains(MaterialState.selected)) {
+    shape:
+        RoundedRectangleBorder(borderRadius: BorderRadius.circular(TSizes.xs)),
+    // checkColor: MaterialStateProperty.resolveWith((states) {
+    checkColor: WidgetStateProperty.resolveWith((states) {
+      // if (states.contains(MaterialState.selected)) {
+      if (states.contains(WidgetState.selected)) {
         return TColors.white;
       } else {
         return TColors.black;
       }
     }),
-    fillColor: MaterialStateProperty.resolveWith((states) {
-      if (states.contains(MaterialState.selected)) {
+    // fillColor: MaterialStateProperty.resolveWith((states) {
+    fillColor: WidgetStateProperty.resolveWith((states) {
+      // if (states.contains(MaterialState.selected)) {
+      if (states.contains(WidgetState.selected)) {
         return TColors.primary;
       } else {
         return Colors.transparent;
